fdf7559f5c12c959d63301634ccec5e078643da9
DK-Creation-DK <81550566+DK-Creation-DK@users.noreply.github.com>
2021-05-19 07:23:04 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -148,10 +148,9 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="GROUP", url="https://t.me/Movie_zoon"),  InlineKeyboardButton(text="CHANNEL", url="https://t.me/Moviezoon12")],
+                    [[InlineKeyboardButton(text="GROUP", url="https://t.me/Viruzhex"),  InlineKeyboardButton(text="CHANNEL", url="https://t.me/Viruzhex_bot")],
 
-                     [InlineKeyboardButton(text="SUPPORT CONTACT", url="https://t.me/cybersecurityhackerjinn") ]]))
-
+                   
     else:
         update.effective_message.reply_text("Iam here")
 
